647f5c7b6ae5bfe605634f37cd4d90798e022d77
rasheedaldeb <rasheedaldeb@gmail.com>
2026-07-17 14:25:51 +0300

    implementing cors

---
+++ src/routes/subjects.ts
@@ -0,0 +1,61 @@
+import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
+import express from "express";
+import { departments, subjects } from "../db/schemas";
+import { db } from "../db";
+const router = express.Router();
+// get all subjects with optional search , filter , pagination
+router.get("/", async (req, res) => {
+  try {
+    const { search, filter, department, page = 1, limit = 10 } = req.query;
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+    const offset = (currentPage - 1) * limitPerPage;
+    const filterConditions = [];
+    // if search is exists , filter by subject name or code
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+    // if department filter is exists , filter by department
+    if (department) {
+      filterConditions.push(ilike(departments.name, `%${department}%`));
+    }
+    // combine all filters using AND if any exist
+    const whereClause =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+    const countResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+    const totalCount = countResult[0]?.count || 0;
+    const subjectsList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: { ...getTableColumns(departments) },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+    return res.status(200).json({
+      data: subjectsList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+export default router;
