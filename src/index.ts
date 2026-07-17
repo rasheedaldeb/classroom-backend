5c22668a8490bf17a4fe173518e1fddde7120b2d
rasheedaldeb <rasheedaldeb@gmail.com>
2026-07-17 16:30:33 +0300

    implementing fixes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import subjectsRouter from "./routes/subjects";
 import cors from "cors";
 const app = express();
 const port = 8000;
+if (process.env.FRONTEND_URL) {
+  throw new Error("frontend url is not set in .env file");
+}
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
